8cce29d2d1caa119d910a63dabfad5b8e46b9c3e
uknow-mac <yoon6763@naver.com>
2023-05-08 19:13:47 +0900

    Member JPA Repository

---
+++ src/main/java/com/shop/spring/shop/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.shop.spring.shop.repository;
+
+import com.shop.spring.shop.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Member findByEmail(String email);
+}
